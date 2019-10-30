b2b838ca20bbdde6f71852ef0ea02ea07a37a62f
lamhidra-hamza <lamhidrahamza@gmail.com>
2019-10-30 15:37:45 +0100

    drone flech control

---
+++ flech_control.py
@@ -0,0 +1,96 @@
+import time
+from dronekit import connect, VehicleMode, LocationGlobalRelative, Command, LocationGlobal
+from pymavlink import mavutil
+import tkinter as tk
+import argparse
+
+def connectMyCopter():
+	parser = argparse.ArgumentParser(description='commands')
+	parser.add_argument('--connect')
+	args = parser.parse_args()
+
+	connection_string = args.connect
+	
+	if not connection_string:
+		import dronekit_sitl
+		sitl = dronekit_sitl.start_default()
+		connection_string = sitl.connection_string()
+
+	vehicle = connect(connection_string, wait_ready=True)
+	vehicle.wait_ready(True, raise_exception=False)
+
+	return vehicle
+
+
+#-- Define arm and takeoff
+def arm_and_takeoff(altitude):
+
+   while not vehicle.is_armable:
+      print("waiting to be armable")
+      time.sleep(1)
+
+   print("Arming motors")
+   vehicle.mode = VehicleMode("GUIDED")
+   vehicle.armed = True
+
+   while not vehicle.armed: time.sleep(1)
+
+   print("Taking Off")
+   vehicle.simple_takeoff(altitude)
+
+   while True:
+      v_alt = vehicle.location.global_relative_frame.alt
+      print(">> Altitude = %.1f m"%v_alt)
+      if v_alt >= altitude - 1.0:
+          print("Target altitude reached")
+          break
+      time.sleep(1)
+      
+ #-- Define the function for sending mavlink velocity command in body frame
+def set_velocity_body(vehicle, vx, vy, vz):
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+            0,
+            0, 0,
+            mavutil.mavlink.MAV_FRAME_BODY_NED,
+            0b0000111111000111, #-- BITMASK -> Consider only the velocities
+            0, 0, 0,        #-- POSITION
+            vx, vy, vz,     #-- VELOCITY
+            0, 0, 0,        #-- ACCELERATIONS
+            0, 0)
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+    
+def ft_goto():
+    a_location = LocationGlobalRelative(-35.3592362, 149.2011166, 30)
+    vehicle.simple_goto(a_location)
+
+def key(event):
+    if event.char == event.keysym:
+        if event.keysym == 'r':
+            print("r pressed >> Set the vehicle to RTL")
+            vehicle.mode = VehicleMode("RTL")
+            
+    else:
+        if event.keysym == 'Up':
+            set_velocity_body(vehicle, gnd_speed, 0, 0)
+        elif event.keysym == 'Down':
+            set_velocity_body(vehicle,-gnd_speed, 0, 0)
+        elif event.keysym == 'Left':
+            set_velocity_body(vehicle, 0, -gnd_speed, 0)
+        elif event.keysym == 'Right':
+            set_velocity_body(vehicle, 0, gnd_speed, 0)
+    
+    
+#---- MAIN FUNCTION
+gnd_speed = 5
+vehicle = connectMyCopter()
+
+#- Takeoff
+arm_and_takeoff(10)
+
+
+#- Read the keyboard with tkinter
+root = tk.Tk()
+print(">> Control the drone with the arrow keys. Press r for RTL mode")
+root.bind_all('<Key>', key)
+root.mainloop()
